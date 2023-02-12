e72c0b41e7170c32e419d7591180c70bf2caf190
RAMESH KUMAR <rameshkumar455555@gmail.com>
2023-02-12 23:44:02 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -64,7 +64,7 @@ class Solution
         return dp[0][0];
     }*/
     //space optimization
-    int solvedp(int n,int a[])
+    /*int solvedp(int n,int a[])
     {
         vector<int>currV(n+1,0);
         vector<int>next(n+1,0);
@@ -84,6 +84,28 @@ class Solution
             }
         }
         return next[0];
+    }*/
+    int solveoptimal(int n,int a[])
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        vector<int>ans;
+        ans.push_back(a[0]);
+        for(int i=1; i<n; i++)
+        {
+            if(a[i]>ans.back())
+            {
+                ans.push_back(a[i]);
+            }
+            else
+            {
+                int index=lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
+                ans[index]=a[i];
+            }
+        }
+        return ans.size();
     }
     //Function to find length of longest increasing subsequence.
     int longestSubsequence(int n, int a[])
@@ -92,7 +114,8 @@ class Solution
        //return solveRec(n,a,0,-1);
        //vector<vector<int>>dp(n,vector<int>(n+1,-1));
        //return solveMem(n,a,0,-1,dp);
-       return solvedp(n,a);
+       //return solvedp(n,a);
+       return solveoptimal(n,a);
     }
 };
 
