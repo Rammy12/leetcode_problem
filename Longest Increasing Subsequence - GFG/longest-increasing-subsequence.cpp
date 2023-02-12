7ae59cccfe24175d1ff19d86a9c7f7dfe1bef521
RAMESH KUMAR <rameshkumar455555@gmail.com>
2023-02-12 23:19:30 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -24,7 +24,7 @@ class Solution
         int exclude=0+solveRec(n,a,curr+1,prev);
         return max(include,exclude);
     }*/
-    int solveMem(int n,int a[],int curr,int prev,vector<vector<int>>&dp)
+    /*int solveMem(int n,int a[],int curr,int prev,vector<vector<int>>&dp)
     {
         //base case
         if(curr==n)
@@ -43,14 +43,34 @@ class Solution
         }
         int exclude=0+solveMem(n,a,curr+1,prev,dp);
         return dp[curr][prev+1]=max(include,exclude);
+    }*/
+    int solvedp(int n,int a[])
+    {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int curr=n-1; curr>=0; curr--)
+        {
+            for(int prev=curr-1; prev>=-1; prev--)
+            {
+                int include=0;
+                //include 
+                if(prev==-1 or a[curr]>a[prev])
+                {
+                    include=1+dp[curr+1][curr+1];
+                }
+                int exclude=0+dp[curr+1][prev+1];
+                dp[curr][prev+1]=max(include,exclude);
+            }
+        }
+        return dp[0][0];
     }
     //Function to find length of longest increasing subsequence.
     int longestSubsequence(int n, int a[])
     {
        // your code here
        //return solveRec(n,a,0,-1);
-       vector<vector<int>>dp(n,vector<int>(n+1,-1));
-       return solveMem(n,a,0,-1,dp);
+       //vector<vector<int>>dp(n,vector<int>(n+1,-1));
+       //return solveMem(n,a,0,-1,dp);
+       return solvedp(n,a);
     }
 };
 
