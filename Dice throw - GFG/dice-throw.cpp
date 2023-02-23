08da9590a8e049294223489703951b07ac573f73
RAMESH KUMAR <rameshkumar455555@gmail.com>
2023-02-23 06:25:20 +0530

    Added solution - LeetHub

---
+++ Dice throw - GFG/dice-throw.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  /*long long solveRec(int dice , int face , int target)
+  {
+      //Base case
+      long long ans=0;
+      if(target<0)
+      {
+          return 0;
+      }
+      if(dice==0 and target!=0)
+      {
+          return 0;
+      }
+      if(target==0 and dice!=0)
+      {
+          return 0;
+      }
+      if(dice==0 && target==0)
+      {
+          return 1;
+      }
+      for(int i=1; i<=face; i++)
+      {
+          ans=ans+solveRec(dice-1,face,target-i);
+      }
+      return ans;
+      
+  }*/
+  long long solveMem(int dice , int face , int target,vector<vector<long long >>&dp)
+  {
+      //Base case
+      long long ans=0;
+      if(target<0)
+      {
+          return 0;
+      }
+      if(dice==0 and target!=0)
+      {
+          return 0;
+      }
+      if(target==0 and dice!=0)
+      {
+          return 0;
+      }
+      if(dice==0 && target==0)
+      {
+          return 1;
+      }
+      if(dp[dice][target]!=-1)
+      {
+          return dp[dice][target];
+      }
+      for(int i=1; i<=face; i++)
+      {
+          ans=ans+solveMem(dice-1,face,target-i,dp);
+      }
+      return dp[dice][target]=ans;
+      
+  }
+    long long noOfWays(int M , int N , int X) {
+        // code here
+        //return solveRec(N,M,X);
+        vector<vector<long long >>dp(N+1,vector<long long>(X+1,-1));
+        return solveMem(N,M,X,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int M,N,X;
+        
+        cin>>M>>N>>X;
+
+        Solution ob;
+        cout << ob.noOfWays(M,N,X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
