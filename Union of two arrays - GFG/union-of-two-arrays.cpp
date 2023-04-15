f935ff133d3fab4d8839a77ef6ad936862196d75
RAMESH KUMAR <rameshkumar455555@gmail.com>
2023-04-15 21:34:14 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -10,8 +10,22 @@ using namespace std;
 class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
+    // time complexity O(n*m)
+    /*int doUnion(int a[], int n, int b[], int m)
+    {
+        int cnt=m+n;
+        for(int i=0;i<n;i++) 
+        {
+            for(int j=0;j<m;j++)
+            {   
+                if(a[i]==b[j])
+                  cnt--;
+            }
+        }
+        return cnt;
+    }*/
+    // time complexity O(n+m)
     int doUnion(int a[], int n, int b[], int m)  {
-        //code here
         unordered_map<int,int>mp;
         for(int i=0; i<n; i++)
         {
